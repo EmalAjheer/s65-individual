d660fa7f42562fee777f0546b9eb9f5ca850f8a0
Ajheer,Emal H <e.ajheer@student.fontys.nl>
2021-06-14 20:53:07 +0200

    added functionalities + docker to other services

--- trend-service/Startup.cs
+++ trend-service/Startup.cs
@@ -44,7 +44,7 @@ namespace trend_service
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "trend_service v1"));
             }
 
-            app.UseHttpsRedirection();
+           // app.UseHttpsRedirection();
 
             app.UseRouting();
 
