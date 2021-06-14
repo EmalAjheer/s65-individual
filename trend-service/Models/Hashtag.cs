a0731211082853e5af9e9cfca14670360de0f36c
Ajheer,Emal H <e.ajheer@student.fontys.nl>
2021-06-14 22:45:34 +0200

    push

---
+++ trend-service/Models/Hashtag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace trend_service.Models
+{
+    public class Hashtag
+    {
+        public Guid Id { get; set; }
+        public Guid TweetId { get; set; }
+        public string HashtagTitle { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ trend-service/Models/HashtagContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace trend_service.Models
+{
+    public class HashtagContext : DbContext
+    {
+        public HashtagContext(DbContextOptions<HashtagContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Hashtag> Hashtag { get; set; }
+    }
+}
